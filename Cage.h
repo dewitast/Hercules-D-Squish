fe9b97490801510c01ac2d61231c605868c3d75d
keziasuhendra <keziasuhendra44@gmail.com>
2017-03-02 12:47:52 +0700

    cage perfecto

--- Cage.h
+++ Cage.h
@@ -2,6 +2,7 @@
 #define CAGE_H
 
 #include "Point.h"
+#include "Animal.h"
 
 /** @class Cage.
   * Kelas Cage merupakan himpunan Cell yang bertipe sama.
@@ -38,13 +39,25 @@ public:
 	/** @brief Menentukan apakah cage penuh.
 	  * @return Mengeluarkan true jika cage penuh.
 	  */
-	boolean IsFull();
+	bool IsFull();
+
+	/** @brief Getter.
+	  */
+	int GetSize();
+	Animal GetAnimal();
+	int GetTotalAnimal();
+
+	/** @brief Setter.
+	  */
+	void AdoptAnimal(Animal*);
+
 
 private:
 	Point* Loc; // himpunan Cell yang terdapat dalam Cage
 	const int MaxSize = 100; // ukuran maksimal Cage
-	int size; //ukuran Cage
-	Animal* a; // Binatang yang terdapat dalam cage tersebut
-}
+	const int size; //ukuran Cage
+	Animal** a; // array of pointer to binatang yang terdapat dalam cage tersebut
+	int TotalAnimal; // jumlah hewan yang ada pada cage
+};
 
 #endif
