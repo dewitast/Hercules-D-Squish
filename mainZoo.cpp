386b421d1dc9868ab986385cf38453a728789da3
calmira <almiratjann@gmail.com>
2017-03-13 17:20:46 +0700

    aaa

--- mainZoo.cpp
+++ mainZoo.cpp
@@ -1,22 +1,20 @@
 #include "Zoo.h"
+#include "Driver.h"
 #include <fstream>
 using namespace std;
 
 int main()
 {
-	Zoo Z;
+	/*Zoo Z;
 	ifstream fin;
 	fin.open("map.txt");
 	fin >> Z;
 	fin.close();
 	Z.ReadAnimal();
-	for(int i=0;i<Z.GetJumlahCage();i++)
-		{
-			for(int j=0;j<(Z.GetCage(i)).GetTotalAnimal();j++)
-			{
-				cout << Z;
-			}
-		}
+	cout << Z;*/
+	Driver D;
+	D.DisplayZoo();
+
 
 	return 0;
 }
