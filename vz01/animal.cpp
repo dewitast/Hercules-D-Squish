25201095f56acb0e58d6c68670bbffebb18da81a
sylviajuliana <sjsylviajuliana@gmail.com>
2017-03-13 21:42:10 +0700

    f

--- vz01/animal.cpp
+++ vz01/animal.cpp
@@ -4,33 +4,6 @@
 #include "animal.h"
 
 Animal::Animal() {}
-Animal::Animal(float w, float f, bool t) {
-  weight = w;
-  food = f;
-  tame = t;
-  SetPoint(-1,-1);
-}
-float Animal::GetWeight() const {
-  return weight;
-}
-float Animal::GetFood() const {
-  return food;
-}
-bool Animal::GetTame() const {
-  return tame;
-}
-Point Animal::GetPos() const {
-  return pos;
-}
-void Animal::SetWeight(float w) {
-  weight = w;
-}
-void Animal::SetFood(float f) {
-  food = f;
-}
-void Animal::SetTame(bool t) {
-  tame = t;
-}
 void Animal::SetPoint(int abs, int ord) {
   pos.SetAbsis(abs);
   pos.SetOrdinat(ord);
--- vz01/animal.h
+++ vz01/animal.h
@@ -17,44 +17,13 @@ public :
     * Menciptakan Animal kosong.
     */
   Animal();
-  /** @brief Constructor dengan parameter.
-    * Menciptakan animal kosong dengan weight w, food f, dan tame t.
-    * @param w Nilai berat binatang.
-    * @param f Nilai jumlah makanan yang dibutuhkan binatang.
-    * @param t Status jinak binatang.
-    */
-  Animal(float w, float f, bool t);
   /** @brief Destructor.
     */
-  virtual ~Animal() {}	
-  /** @brief Getter untuk weight.
-    * @return Mengembalikan nilai weight binatang.
-    */
-  float GetWeight() const;
-  /** @brief Getter untuk food.
-    * @return Mengembalikan nilai food binatang.
-    */
-  float GetFood() const;
-  /** @brief Getter untuk tame.
-    * @return Mengembalikan status tame binatang.
-    */
-  bool GetTame() const;
+  virtual ~Animal() {}
   /** @brief Getter untuk pos.
     * @return Mengembalikan nilai lokasi binatang.
     */
   Point GetPos() const;
-  /** @brief Setter untuk weight.
-    * @param w Nilai weight yang akan di-set pada animal.
-    */
-  void SetWeight(float w);
-  /** @brief Setter untuk food.
-    * @param f Nilai food yang akan di-set pada animal.
-    */
-  void SetFood(float f);
-  /** @brief Setter untuk tame.
-    * @param t Status tame yang akan di-set pada animal.
-    */
-  void SetTame(bool t);
   /** @brief Setter point dengan parameter dua buah nilai integer.
     * @param abs Nilai absis yang akan di-set pada lokasi animal.
     * @param ord Nilai ordinat yang akan di-set pada lokasi animal.
@@ -82,15 +51,6 @@ public :
     */
   virtual char Render() = 0;
 protected:
-  /** @brief Berat binatang.
-    */
-  float weight;
-  /** @brief Jumlah makanan binatang.
-    */
-  float food;
-  /** @brief Status jinak binatang.
-    */
-  bool tame;
   /** @brief Lokasi binatang.
     */
   Point pos;
