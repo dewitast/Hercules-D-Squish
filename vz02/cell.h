f987c5599fbd82c4f585cb7100ba597657eab1aa
sylviajuliana <sjsylviajuliana@gmail.com>
2017-03-14 01:04:02 +0700

    k

--- vz02/cell.h
+++ vz02/cell.h
@@ -4,71 +4,45 @@
 #ifndef CELL_H
 #define CELL_H
 
-#include "renderable.h"
+#include "../renderable/renderable.h"
 
 /** @class Cell.
-  * Kelas abstrak Cell merupakan simulasi dari petak-petak yang terdapat dalam kebun binatang.
+  * Kelas Cell merupakan simulasi dari petak-petak yang terdapat dalam kebun binatang.
   */
-class Cell : public Renderable{
-public:
-	/** @brief render.
-	  * @return Mengembalikan char yang merupakan representasi objek yang dicetak.
-	  */
-	virtual char Render() = 0;
-
-	/** @brief clone.
-	  * Melakukan cloning untuk menciptakan objek baru.
-	  * @return Mengeluarkan pointer to cell objek tersebut.
-	  */
-	virtual Cell* Clone() const = 0;
-
-	/** @brief IsLandHabitat.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Land Habitat.
-	  */
-	virtual bool IsLandHabitat() = 0;
-
-	/** @brief IsWaterHabitat.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Water Habitat.
-	  */
-	virtual bool IsWaterHabitat() = 0;
-
-	/** @brief IsAirHabitat.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Air Habitat.
-	  */
-	virtual bool IsAirHabitat() = 0;
-
-	/** @brief IsRoad.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Road.
-	  */
-	virtual bool IsRoad() = 0;
-
-	/** @brief IsPark.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Park.
-	  */
-	virtual bool IsPark() = 0;
-
-	/** @brief IsRestaurant.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Restaurant.
-	  */
-	virtual bool IsRestaurant() = 0;
-
-	/** @brief IsExit.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Exit.
-	  */
-	virtual bool IsExit() = 0;
-
-	/** @brief IsEntrance.
-	  * @return Menghasilkan true jika code pada layar merupakan kode Entrance.
-	  */
-	virtual bool IsEntrance() = 0;
-
-	/** @brief IsHabitat.
+class Cell {
+public :
+  /** @brief Constructor.
+    * Menciptakan Cell kosong.
+    */
+  Cell();
+  /** @brief Constructor dengan parameter.
+    * Menciptakan Cell kosong dengan tipe objek ot dan render r.
+    * @param ot Object type dari Cell.
+    * @param r Render dari Cell.
+    */
+  Cell(string ot);
+  /** @brief Destructor.
+    */
+  ~Cell();
+  /** @brief Getter untuk object type.
+    * @return Mengembalikan object type dari Cell.
+    */
+  string GetObjectType() const;
+  /** @brief Getter untuk render.
+    * @return Mengembalikan render dari Cell.
+    */
+  char GetRender() const;
+  /** @brief Setter untuk object type dari Cell.
+    * @param ot Nilai object type yang akan di-set pada Cell.
+    */
+  void SetObjectType(string ot);
+  /** @brief IsHabitat.
 	  * @return Menghasilkan true jika code pada layar merupakan kode Land, Air, atau Water Habitat.
 	  */
-	bool IsHabitat() {return ((IsLandHabitat())||(IsWaterHabitat())||(IsAirHabitat()));}
-
-protected:
-
+  bool IsHabitat();
+private :
+  string object_type;
+  char render;
 };
 
 #endif
